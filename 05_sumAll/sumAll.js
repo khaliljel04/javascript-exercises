f63a802e516a6d983edb4fc38e91a2bd356d2eda
Khalil <111660184+khaliljel04@users.noreply.github.com>
2023-02-01 13:21:05 +0100

    Complete exercice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (
+        !Number.isInteger(first) ||
+        !Number.isInteger(second) ||
+        first <= 0 ||
+        second <=0
+    ){
+        return 'ERROR';
+    }
+    return (Math.abs(first - second) + 1) / 2 * (first + second);
 };
 
 // Do not edit below this line
