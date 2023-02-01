200835811b1c0c2229967cf03e7a324fdfb6f718
Khalil <111660184+khaliljel04@users.noreply.github.com>
2023-02-01 14:01:40 +0100

    Complete exercice 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let s = 0;
+  arr.forEach((num) => s += num);
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let p = 1;
+  arr.forEach((num) => p *= num);
+  return p;
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+	return x**n;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) return 1;
+  let f = 1;
+  for (; a > 1; a--){
+    f *= a;
+  }
+  return f;
 };
 
 // Do not edit below this line
