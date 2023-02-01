a24b475058c1d1a85fa899d54c9686a6ea390cb4
Khalil <111660184+khaliljel04@users.noreply.github.com>
2023-02-01 17:58:39 +0100

    Complete exercice 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if (num < 0) return 'OOPS';
+    let a = 1;
+    let b = 1;
+    for (let i = 2; i < num; i++) {
+        [a, b] = [b, a+b];
+    }
+    return b;
 };
 
 // Do not edit below this line
