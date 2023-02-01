31eca49aa12026c38740705982ca1411022d2138
Khalil <111660184+khaliljel04@users.noreply.github.com>
2023-02-01 11:59:21 +0100

    Complete exercice 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) return 'ERROR'
+    let phrase = '';
+    for (let i = 0; i < num; i++){
+        phrase += word;
+    }
+    return phrase;
 };
 
 // Do not edit below this line
