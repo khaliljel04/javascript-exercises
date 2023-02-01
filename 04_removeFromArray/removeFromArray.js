b6f658d8482028bbadafecb660bb941c6e758227
Khalil <111660184+khaliljel04@users.noreply.github.com>
2023-02-01 12:46:43 +0100

    Complete exercice 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    const elements = Array.from(arguments).slice(1);
+    for (const element of elements) {
+        if (!arr.includes(element)) continue;
+        arr.splice(arr.indexOf(element), 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
