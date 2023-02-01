681a8093a6b2cbd842f316f7a85dc85e2aa003a4
Khalil <111660184+khaliljel04@users.noreply.github.com>
2023-02-01 17:50:37 +0100

    Complete exercice 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    phrase = phrase.toLowerCase();
+    phrase = phrase.split('')
+    let i = 0;
+    while (i < phrase.length) {
+        if (/^[a-z]+$/.test(phrase[i])) {
+            i++;
+            continue;
+        }
+        phrase.splice(i,1);
+    }
+    return (phrase.join('') === phrase.reverse().join(''));
 };
 
 // Do not edit below this line
