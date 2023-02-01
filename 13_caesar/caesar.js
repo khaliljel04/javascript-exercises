121cdd1f98088eaed44c38aee6edb91163f7c6bf
Khalil <111660184+khaliljel04@users.noreply.github.com>
2023-02-01 19:03:24 +0100

    Complete exercice 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,16 @@
-const caesar = function() {
-
+const caesar = function(phrase, key) {
+    key %= 26;
+    if (key < 0) key += 26;
+    phrase = phrase.split('');
+    for (let i = 0; i < phrase.length; i++) {
+        if (/[a-z]/.test(phrase[i])) {
+            phrase[i] = String.fromCharCode('a'.charCodeAt() + (phrase[i].charCodeAt() - 'a'.charCodeAt() + key) % 26);
+        }
+        if (/[A-Z]/.test(phrase[i])) {
+            phrase[i] = String.fromCharCode('A'.charCodeAt() + (phrase[i].charCodeAt() - 'A'.charCodeAt() + key) % 26);
+        }
+    }
+    return phrase.join('');
 };
 
 // Do not edit below this line
